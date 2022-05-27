dd2289474fb6a3388ed10ede81a357ef9c215979
Anton <imewokrus@gmail.com>
2022-05-27 20:59:45 +0300

    First commit

--- src/main/java/pro/sky/java/course2/controller/FirstController.java
+++ src/main/java/pro/sky/java/course2/controller/FirstController.java
@@ -1,4 +1,42 @@
 package pro.sky.java.course2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.java.course2.service.Calculations;
+import pro.sky.java.course2.service.Calculationsimpl;
+
 @RestController
-public class FirstControler {
+@RequestMapping("/calculator")
+public class FirstController {
+
+    private final Calculations calculations;
+
+public FirstController(Calculations calculations) {
+    this.calculations = calculations;
+}
+    @GetMapping("/plus")
+    public String showAddition(@RequestParam (required = true) String num1, @RequestParam (required = true) String num2) {
+        return calculations.getAddition(num1,num2);
+    }
+
+    @GetMapping("/minus")
+    public String showSubtraction(@RequestParam (required = true) String num1, @RequestParam (required = true) String num2) {
+        return calculations.getSubtraction(num1,num2);
+    }
+
+    @GetMapping("/multiply")
+    public String showMultiplication(@RequestParam (required = true) String num1, @RequestParam (required = true) String num2) {
+        return calculations.getMultiplication(num1, num2);
+    }
+
+    @GetMapping("/divide")
+    public String showDivision(@RequestParam (required = true) String num1, @RequestParam (required = true) String num2) {
+        return calculations.getDivision(num1, num2);
+    }
+    @GetMapping("/calculator")
+    public String greetings() {
+        return "<b>Добро пожаловать в калькулятор</b>";
+    }
 }
---
+++ src/main/java/pro/sky/java/course2/service/Calculations.java
@@ -0,0 +1,8 @@
+package pro.sky.java.course2.service;
+
+public interface Calculations {
+    String getAddition(String num1, String num2);
+    String getSubtraction(String num1, String num2);
+    String getDivision(String num1, String num2);
+    String getMultiplication(String num1, String num2);
+}
